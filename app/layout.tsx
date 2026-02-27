db86c068fc34be45e004c5c1609d79be244863db
Joel Fernandes <joel.f.2003@hotmail.com>
2026-02-27 15:58:17 +0000

    feat: add JPG open graph image and update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     siteName: siteConfig.name,
     images: [
       {
-        url: "/og-image.png",
+        url: "/og-image.jpg",
         width: 1200,
         height: 630,
         alt: siteConfig.name,
